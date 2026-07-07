643c8485dca784d92d7798a6e21981ab7f850802
@josearpaiaq <josearpaiaquintero@gmail.com>
2026-07-07 16:27:41 -0500

    fix: update console log message for clarity in server startup

--- javascript/nodejs/tasks-crud/index.js
+++ javascript/nodejs/tasks-crud/index.js
@@ -17,5 +17,5 @@ app.use("/", router);
 app.use(errorMiddleware);
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Listening on port ${port}`);
 });
